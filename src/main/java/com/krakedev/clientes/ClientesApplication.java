adefb675cad98947c43983a5be5eb61c4cb04008
angel-hub31 <moralesmales1@hotmail.com>
2026-05-23 17:02:09 -0500

    clase

--- src/main/java/com/krakedev/clientes/ClientesApplication.java
+++ src/main/java/com/krakedev/clientes/ClientesApplication.java
@@ -2,8 +2,15 @@ package com.krakedev.clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.krakedev" )
+
+@EnableJpaRepositories(basePackages="com.krakedev")
+
+@EntityScan(basePackages ="com.krakedev")
+
 public class ClientesApplication {
 
 	
